b91a74368a27972d539210d4bd88a5096c72993b
Bryce Claughton <bclaught@gmail.com>
2023-06-06 13:20:29 -0400

    import specific requests functions and classes

--- nhldeez/api.py
+++ nhldeez/api.py
@@ -1,4 +1,4 @@
-import requests as r
+from requests import get, Response
 from urllib.parse import urljoin
 
 BASEURL = "https://statsapi.web.nhl.com/"
@@ -8,36 +8,36 @@ def build_url(*args) -> str:
     return urljoin(BASEURL, "/".join(["api", "v1", *args]))
 
 
-def conferences(id: int = None) -> r.Response:
-    return r.get(build_url("conferences", "" if id is None else str(id)))
+def conferences(id: int = None) -> Response:
+    return get(build_url("conferences", "" if id is None else str(id)))
 
 
-def divisions(id: int = None) -> r.Response:
-    return r.get(build_url("divisions", "" if id is None else str(id)))
+def divisions(id: int = None) -> Response:
+    return get(build_url("divisions", "" if id is None else str(id)))
 
 
-def draft(year: int = None) -> r.Response:
-    return r.get(build_url("draft", "" if year is None else str(year)))
+def draft(year: int = None) -> Response:
+    return get(build_url("draft", "" if year is None else str(year)))
 
 
-def draft_prospects(id: int = None) -> r.Response:
-    return r.get(build_url("draft/prospects", "" if id is None else str(id)))
+def draft_prospects(id: int = None) -> Response:
+    return get(build_url("draft/prospects", "" if id is None else str(id)))
 
 
-def game_boxscore(game_id: int) -> r.Response:
-    return r.get(build_url("game", str(game_id), "boxscore"))
+def game_boxscore(game_id: int) -> Response:
+    return get(build_url("game", str(game_id), "boxscore"))
 
 
-def game_content(game_id: int) -> r.Response:
-    return r.get(build_url("game", str(game_id), "content"))
+def game_content(game_id: int) -> Response:
+    return get(build_url("game", str(game_id), "content"))
 
 
-def game_feed(game_id: int) -> r.Response:
-    return r.get(build_url("game", str(game_id), "feed", "live"))
+def game_feed(game_id: int) -> Response:
+    return get(build_url("game", str(game_id), "feed", "live"))
 
 
-def get_schedule(season: int, team_id: int = None, game_type: str = None) -> r.Response:
-    return r.get(
+def get_schedule(season: int, team_id: int = None, game_type: str = None) -> Response:
+    return get(
         build_url("schedule"),
         params={
             "season": f"{season}{season+1}",
